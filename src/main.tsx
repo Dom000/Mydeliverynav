dd5959c8ff342b5ad776f7e4c2c672455c6d1214
dom000 <godknowsegiboy00@gmail.com>
2026-07-16 23:03:43 +0100

    refactor: remove EmptyState component and BrowserRouter from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "sonner";
 import "./index.css";
-import EmptyState from "./components/EmptyState.tsx";
-
+import App from "./App";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <EmptyState />
-      </BrowserRouter>
+      <App />
       <Toaster />
     </QueryClientProvider>
   </StrictMode>,
